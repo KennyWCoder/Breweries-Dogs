81418e92d7d6f4ba3a3a3bd33d07666506044798
Tracy Pham <phamtracy95@gmail.com>
2022-09-26 21:18:41 -0700

    Added code for API, displays API data

--- asset/index.js
+++ asset/index.js
@@ -1,40 +1,94 @@
-// Brewery API
-var breweryAPIURL = "https://api.openbrewerydb.org/breweries?by_city" + city "&per_page=5";
+// Initialize city and state
+var city = "";
+var state = "";
 
-// Random Dog photo API
-var dogAPIURL = "https://dog.ceo/api/breeds/image/random";
+// Search button
+var searchBtn = document.getElementById("search-btn");
 
-// Initialize city
-var city="";
+var breweryList = document.querySelectorAll(".brewery-name");
 
-// TODO: Add event listener to run these two fetch requests when search button is clicked 
+// Searches for breweries and displays dog photo when search button is clicked
+searchBtn.addEventListener("click", function (event) {
+  event.preventDefault();
 
-// Basic fetch request for Brewery API
-fetch(breweryAPIURL)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-})
-.catch(function (error) {
-  alert("Please enter a valid city name");
-  return; 
+  // Gets the value of user input city
+  city = document.getElementById("search-city").value;
+  state = document.getElementById("search-state").value;
+  console.log(city);
+  console.log(state);
+  fetchBreweries();
+  fetchDogs();
 });
 
-// Basic fetch request for Random Dog API
-fetch(dogAPIURL)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-})
-.catch(function (error) {
-  alert("Please enter a valid city name");
-  return;
-});
+var breweryName = document.getElementsByClassName("brewery-name");
+var breweryAddress = document.getElementsByClassName("address");
+var breweryCityStateZip = document.getElementsByClassName("city");
+var breweryPhone = document.getElementsByClassName("phone");
+var breweryWebsite = document.getElementsByClassName("website");
 
+// Fetches breweries
+function fetchBreweries() {
+  // Brewery API URL
+  var breweryAPIURL =
+    "https://api.openbrewerydb.org/breweries?by_state=" +
+    state +
+    "&by_city=" +
+    city +
+    "&per_page=5";
+
+  console.log(breweryAPIURL);
+  fetch(breweryAPIURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      for (var i = 0; i < breweryList.length; i++) {
+        console.log(data.name);
+        breweryName[i].textContent = data[i].name;
+        breweryAddress[i].textContent = data[i].street;
+        breweryCityStateZip[i].textContent =
+          data[i].city + ", " + data[i].state + ", " + data[i].postal_code;
+        breweryPhone[i].textContent = data[i].phone;
+        breweryWebsite[i].textContent = data[i].website_url;
+
+        breweryWebsite[i].setAttribute("href", data[i].website_url);
+      }
+    });
+  // .catch(function (error) {
+  //   alert("Please enter a valid city name");
+  //   return;
+  // });
+}
+
+// Dog photo image section
+var dogPhoto = document.getElementById("dog-photo");
+
+// Fetches random dog photo
+function fetchDogs() {
+  // Random Dog photo API URL
+  var dogAPIURL = "https://dog.ceo/api/breeds/image/random";
+
+  fetch(dogAPIURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.message);
+      dogPhoto.setAttribute("src", data.message);
+      dogPhoto.setAttribute("alt", "cute dog photo");
+    });
+
+  // might not need this since there wont be errors
+  // .catch(function (error) {
+  //   alert("Please enter a valid city name");
+  //   return;
+  // });
+}
 
 // TODO: Add function for local storage to save city searches OR to save specific brewerires
 // TODO: when saved searches are clicked, it goes to that search results
+// TODO: add if statement so that user always has to enter a city because if no city is entered but search button is clicked, it'll still return some random data
+// TODO: hide dog photo
